9556b31511d51f9ca1a36bf17b0eb62cd81a1b81
Sindre Bore <sindre.fromreide@gmail.com>
2019-05-28 00:51:29 +0200

    legger til .js .css og .html filene brukt til login/registrerings systemet

---
+++ Login_Sindre/login.js
@@ -0,0 +1,71 @@
+let users = [
+    {name: "Sindre", password: "Password123"},
+    {name: "Bengt", password: "h4x0r1337"},
+    {name: "test", password: "test1234"}
+];
+
+//DOM-element variables
+let usernameInputElement = document.getElementById("usernameInput");
+let passwordInputElement = document.getElementById("passwordInput");
+let loginBtnElement = document.getElementById("loginBtn");
+let registerUsernameInputElement = document.getElementById("registerUsernameInput");
+let registerPasswordInputElement = document.getElementById("registerPasswordInput");
+let confirmPasswordInputElement = document.getElementById("confirmPasswordInput");
+let registerBtnElement = document.getElementById("registerBtn");
+let loginToRegisterElement = document.getElementById("loginToRegister");
+let registerToLoginElement = document.getElementById("registerToLogin");
+let loginInterfaceElement = document.getElementById("loginInterface");
+let registerInterfaceElement = document.getElementById("registerInterface");
+
+//Checks the value of the login inputs against the objects in the users array
+loginBtnElement.addEventListener("click", function(){
+    
+    let credentialCheck = users.find(o => o.name === usernameInputElement.value && o.password === passwordInputElement.value);
+    
+    
+    
+    if(credentialCheck){
+        usernameInputElement.value = "Success";
+        passwordInputElement.value = "";
+    } else {
+        usernameInputElement.value = "Try again";
+        passwordInputElement.value = "";
+    }
+    
+}) 
+
+//Register a new user
+registerBtnElement.addEventListener("click", function(){
+    
+    let isUserTaken = users.find(o => o.name === registerUsernameInputElement.value);
+    
+    //Here we check if the password is the same in both input fields, that both username and password are within the character limit, and that the username is not already in use
+    if(registerPasswordInputElement.value === confirmPasswordInputElement.value 
+       && registerPasswordInputElement.value.length >= 6 
+       && registerPasswordInputElement.value.length <= 18 
+       && registerUsernameInputElement.value.length <= 20
+       && !isUserTaken){
+        users[users.length] = {name: registerUsernameInputElement.value, password: registerPasswordInputElement.value}
+        
+        registerUsernameInputElement.value = "User created"
+    } else{
+        registerUsernameInputElement.value = "An error occured";
+    }
+})
+
+loginToRegisterElement.addEventListener("click", function(){
+    
+    loginInterfaceElement.style.display = "none";
+    registerInterfaceElement.style.display = "block";
+})
+
+registerToLoginElement.addEventListener("click", function(){
+    
+    loginInterfaceElement.style.display = "block";
+    registerInterfaceElement.style.display = "none";
+})
+
+
+
+
+

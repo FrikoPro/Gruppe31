da09e3dffa5096b91978441528f0269417d9d214
Fredrik Jansen <jansenfredrik@gmail.com>
2019-05-28 23:04:44 +0200

    medlemmer kan dras til kortene, og klones. men må utvide funksjonalitet med at samme person kan ikke legges på flere ganger

--- gruppe31_app/js/dashboard.js
+++ gruppe31_app/js/dashboard.js
@@ -1,2 +1,50 @@
 // dashboard.js
 // Javascript for the dashboard
+
+// users array
+var users = document.getElementsByClassName("users");
+
+// grabbing all the available cards
+var cards = document.getElementsByClassName("card");
+
+// makes the elements draggable
+for (var i=0; i<users.length; i++) {
+    users[i].setAttribute("draggable", true);
+}
+
+// creates a list of the users
+var userList = [];
+
+// pushing userId into userList
+for (var i=0; i<users.length; i++) {
+    userList.push(users[i].getAttribute("id"));
+}
+
+// references to the ID of card and user
+var cardId;
+var userId;
+
+
+// adding eventListeners to cards
+for (var i=0; i<cards.length; i++) {
+    cards[i].addEventListener("dragover", e => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "move";
+        cardId = e.target.id;
+    });
+    cards[i].addEventListener("drop", e => {
+        console.log("DROP", e);
+                var user = document.getElementById(userId);
+                var card = document.getElementById(cardId);
+                var cln = user.cloneNode(true);
+                card.appendChild(cln);
+    });
+}
+
+// putting id's on the users
+for(var i =0; i<users.length; i++){
+    users[i].addEventListener("dragstart", e => {
+        console.log("dragstart", e);
+        userId = e.target.id;
+    });
+}

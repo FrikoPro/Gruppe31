40281026c4fcd7924b9c83b18b5ea2bcff989e5a
burtmaclintheblackroses@gmail.com <burtmaclintheblackroses@gmail.com>
2019-06-03 16:01:47 +0200

    Darkmode Update

--- gruppe31_app/js/darkmode.js
+++ gruppe31_app/js/darkmode.js
@@ -12,9 +12,12 @@ darkmodeIcon.addEventListener("click", () =>{
     if(!darkmode) {
         body.style.backgroundColor = "#444444";
         body.style.color = "#fff";
+        darkmodeIcon.style.color = "#ffffff";
         darkmode = true;
     } else {
         body.style.backgroundColor = "#ffffff";
+        body.style.color = "#444444";
+        darkmodeIcon.style.color = "#000000";
         darkmode = false;
     }
 })
